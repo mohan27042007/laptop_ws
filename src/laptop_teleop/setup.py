929e31f63c124a5e4ab5656e37bc9ee67145ec16
mohan27042007 <mail4mohan27@gmail.com>
2025-12-01 11:22:05 +0530

    Run this in laptop

---
+++ src/laptop_teleop/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'laptop_teleop'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/teleop.launch.py']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mohanarangan-t-r',
+    maintainer_email='mail4mohan27@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
